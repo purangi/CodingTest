e413546c30484e0f5ff19a5bfa17e9ee089b1cfb
김유원 <68212670+purangi@users.noreply.github.com>
2024-01-16 09:27:20 +0900

    [level Lv.1] Title: 콜라츠 추측, Time: 0.17 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        
+        while(num != 1) {
+            if(++answer > 500) return -1;
+            num = (num % 2 == 0) ? num / 2 : (num * 3) + 1;
+        }
+        
+        return answer;
+    }
+}
