b1d156dfc595826687a8ee95bb367a006b614781
김유원 <68212670+purangi@users.noreply.github.com>
2024-01-18 09:27:37 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.04 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 개수와 덧셈 - 77884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884?language=cpp) 
 
 ### 성능 요약
 
-메모리: 31.5 MB, 시간: 0.25 ms
+메모리: 4.07 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 9:22:38
+2024년 1월 4일 9:27:36
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool divisor(int num){
+    int cnt = 0;
+    for(int i = 1; i <= num; i++) {
+        if(num % i == 0) cnt++;
+    }
+        
+    bool answer = cnt % 2 == 0 ? true : false;
+    return answer;
+}
+    
+int solution(int left, int right) {
+    int answer = 0;
+        
+    for(int i = left; i <= right; i++) {
+        answer += divisor(i) ? i : -i;
+    }
+        
+    return answer;
+}
