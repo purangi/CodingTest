a34b7596ca69030a58b2a23dc7e0d266eb73ac69
김유원 <68212670+purangi@users.noreply.github.com>
2024-01-25 09:08:54 +0900

    [level 1] Title: 최소직사각형, Time: 0.47 ms, Memory: 32.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Solution {
+    public int solution(int[,] sizes) {
+        int answer = 0;
+        
+        for(int i = 0; i < sizes.GetLength(0); i++) {
+            if(sizes[i, 0] < sizes[i,1]){
+                int temp = sizes[i,1];
+                sizes[i,1] = sizes[i,0];
+                sizes[i,0] = temp;
+            }
+        }
+        
+        int xMax = 0;
+        int yMax = 0;
+        
+        for(int i = 0; i < sizes.GetLength(0); i++) {
+            if(sizes[i,0] > xMax) xMax = sizes[i,0];
+            if(sizes[i,1] > yMax) yMax = sizes[i,1];
+        }
+        
+        return xMax * yMax;
+    }
+}
