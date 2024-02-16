264d8c9d5bfe2dfd2d150cf4456fec9bab9b63ab
김유원 <68212670+purangi@users.noreply.github.com>
2024-02-16 09:41:25 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.25 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cs
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cs
@@ -1,27 +1,22 @@
 using System;
 
 public class Solution {
-    int Divisor(int num) {
-        int cnt = 0;
-        for(int i = 1; i *i <= num; i++) {
-            if(num % i == 0) {
-                cnt++;
-                
-                if(i*i < num) cnt++;
-            }
-        }
-        
-        return cnt;
-    }
     public int solution(int number, int limit, int power) {
         int answer = 0;
-        
-        for(int i = 1; i <= number; i++) {
-            int temp = Divisor(i);
-            if(temp > limit) answer += power;
-            else answer += temp;
+        int[] count = new int[number+1];
+        for(int i=1;i<=number;i++)
+        {
+            for(int j=i;j<=number;j+=i)
+            {
+                count[j]++;
+            }
         }
-        
+        for(int i=1;i<=number;i++)
+        {
+            if(count[i] > limit) count[i] = power;
+            answer += count[i];
+        }
+
         return answer;
     }
 }
