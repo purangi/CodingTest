38607a300a5053b1ad867debe425dc953abd9557
김유원 <68212670+purangi@users.noreply.github.com>
2024-03-26 09:34:07 +0900

    [level 2] Title: H-Index, Time: 1.91 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        
+        Array.Sort(citations);
+        Array.Reverse(citations);
+        
+        for(int i = 0; i < citations.Length; i++) {
+            if((i+1) <= citations[i]) answer++;
+        }
+        return answer;
+    }
+}
