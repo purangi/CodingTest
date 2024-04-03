b1e67f7b1fae36dde7f52cf3afbeaad2a64e671f
김유원 <68212670+purangi@users.noreply.github.com>
2024-04-03 09:44:42 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    queue<int> printer;
+    vector<int> sorted;
+    for(int i=0; i<priorities.size(); i++) {
+        printer.push(i);
+    }
+    while(!printer.empty()) {
+        int now_index = printer.front();
+        printer.pop();
+        if(priorities[now_index] != *max_element(priorities.begin(),priorities.end())) {
+            printer.push(now_index);
+        } else {
+            sorted.push_back(now_index);
+            priorities[now_index] = 0;
+        }
+    }
+    for(int i=0; i<sorted.size(); i++) {
+        if(sorted[i] == location) return i+1;
+    }
+}
